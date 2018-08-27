f0926a8c3ac8d579ae35f1ceb70efb5b6e9fca68
Patrick McCarty <patrickthesailorman@gmail.com>
2018-08-27 22:04:59 +0000

    Data types

--- basics/variables.rb
+++ basics/variables.rb
@@ -9,3 +9,24 @@ MyNum = 42
 
 MyNum = 8
 # warning: already initialized constant MyNum
+
+# Data Types
+
+=begin All variables in Ruby can be of all data types. 
+Ruby automatically determines data type by the value assigned to the variable. 
+For example:
+=end
+
+x = 42 # integer
+y = 1.58 # floating point value
+z = "Hello" # string
+
+=begin You can reassign a different value to a variable at any time.
+To insert the value of a variable into a double quote string (a string is a sequence of characters, such as "Hello"), use the # symbol and curly brackets with the variable name. 
+For example:
+=end 
+
+age = 42
+puts "He is #{age} years old"
+
+# outputs "He is 42 years old"

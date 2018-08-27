57835672704593aa1f8ac58b86b2e192723121f3
Patrick McCarty <patrickthesailorman@gmail.com>
2018-08-27 21:41:06 +0000

    Add const

---
+++ basics/variables.rb
@@ -0,0 +1,11 @@
+=begin
+Variables beginning with a capital letter are called constants.
+The value of a constant variable cannot be changed once it has been assigned.
+For example:
+=end
+
+MyNum = 42
+# Trying to change the value produces a warning
+
+MyNum = 8
+# warning: already initialized constant MyNum

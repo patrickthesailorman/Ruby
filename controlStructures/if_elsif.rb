cb9ef319a006e76df81f92e37134c19962ca7837
Patrick McCarty <patrickthesailorman@gmail.com>
2018-08-28 02:39:31 +0000

    Add if_elsif

---
+++ controlStructures/if_elsif.rb
@@ -0,0 +1,13 @@
+# if Statements
+
+=begin You can use an if expression to execute code when a certain condition holds. 
+If a conditional expression evaluates to true, the if code is carried out. Otherwise, the code is ignored. 
+For example:
+=end
+a = 42
+
+if a > 7
+  puts "Yes"
+end
+
+# outputs "Yes"

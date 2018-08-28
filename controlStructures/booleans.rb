8abcd05b0476e47a06a703d5456306325bf23e4e
Patrick McCarty <patrickthesailorman@gmail.com>
2018-08-28 00:45:51 +0000

    Add !=

--- controlStructures/booleans.rb
+++ controlStructures/booleans.rb
@@ -9,6 +9,7 @@ Only true and false are Booleans. nil is not a Boolean. 0 is not a Boolean.
 The string "Hello" is not a Boolean. 
 However, in a context where a Boolean is expected, Ruby evaluates them as Boolean (truthy and falsey).
 =end
+
 # Comparison
 
 # A Boolean comparison using the == operator returns true when two operands are equal, and false when they are not:
@@ -19,4 +20,11 @@ puts a == b # false
 
 puts a == 5 # true
 
-# Be careful not to confuse assignment (one equals sign) with comparison (two equals signs).
+# Be careful not to confuse assignment (one equals sign) with comparison (two equals signs
+
+# Another comparison operator, the not equal operator (!=), evaluates to true if the items being compared aren't equal, and false if they are. 
+# For example:
+a = 8
+b = 7
+
+puts a != b # true

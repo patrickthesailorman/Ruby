b9d16d952b79ffb3bd35946001fb8ed4f8f01807
Patrick McCarty <patrickthesailorman@gmail.com>
2018-08-29 00:54:00 +0000

    Add methods

---
+++ methods/callingMethods.rb
@@ -0,0 +1,32 @@
+# Methods
+
+=begin In the previous lessons we have seen some examples of methods, such as the reverse method for arrays.
+A method is a set of statements that perform a specific task. 
+You can define your own methods to perform your desired tasks.
+The def keyword is used to define a method.
+=end
+
+def say
+  puts "Hi"
+end
+
+# The above code defines a method called "say" that performs a simple output operation, printing "Hi".
+# The method name should start with a lowercase letter, so it will not be confused with constants.
+
+# Calling Methods
+
+# Now that we have defined our method, we can call it by including its name in a line of code:
+def say
+  puts "Hi"
+end
+
+say
+# outputs "Hi"
+
+# You can call methods as many times as you need:
+say
+say
+say
+
+# Methods should be defined before calling them, otherwise Ruby will raise an error.
+# You can also call methods inside other methods.

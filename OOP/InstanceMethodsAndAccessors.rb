2e4fa4ce616a80ed8506dce43daf56ce3338413b
Patrick McCarty <patrickthesailorman@gmail.com>
2018-08-30 00:55:18 +0000

    Add accessors

--- OOP/InstanceMethodsAndAccessors.rb
+++ OOP/InstanceMethodsAndAccessors.rb
@@ -21,3 +21,29 @@ d.bark
 # outputs "Woof"
 
 # As with any method, instance methods can include multiple parameters and return values.
+
+# Accessors
+
+# An instance method can also be created to access the instance variables from outside of the object.
+# For example, if we want to access the @name instance variable for a Person object, we need a method that returns the value of that variable:
+class Person
+  def initialize(name)
+    @name = name
+  end
+  def get_name
+    @name
+  end
+end
+
+p = Person.new("David")
+puts p.get_name
+
+# outputs "David"
+
+=begin
+We created an instance method get_name that returns the value of the @name instance variable and then called it for our object p.
+Getter and setter methods are called accessors.
+The method that is used to retrieve the value of the variable is called a getter method (get_name in our example).
+The method that is used to modify the value of the variable is called a setter method.
+=end
+

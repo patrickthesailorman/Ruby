a20ae1941a9a2e62c11552bcc7a423c4e69e2ab1
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-02 20:41:21 +0000

    Add simplegame

--- OOP/simpleGame.rb
+++ OOP/simpleGame.rb
@@ -29,3 +29,28 @@ hit hits the opponent (decreases the opponent's health by the amount of player's
 to_s outputs the player information.
 We have also defined getter and setter accessors for the instance variables using the attr_accessor method.
 =end
+
+
+# With the Player class defined, we can now define a method to make two opponents fight:
+def fight(p1, p2)
+  while p1.isAlive && p2.isAlive
+    p1.hit(p2)
+    p2.hit(p1)
+    show_info(p1, p2)
+  end
+    
+  if p1.isAlive 
+    puts "#{p1.name} WON!" 
+  elsif p2.isAlive
+    puts "#{p2.name} WON!" 
+  else
+    puts "TIE!"
+  end
+end
+
+def show_info(*p)
+  p.each { |x| puts x}
+end
+
+# The fight method uses a loop to make the players hit each other until the isAlive method returns false for one of them. After each iteration, the information of both players is output to the screen using the show_info method we defined.
+# Once the loop is over (meaning one of the opponents has lost), we check and output the corresponding result.
